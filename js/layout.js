83431d1c66e79406d49ddc5a1da9823796e9b6fd
Jiadong Wu <wuplus1992@gmail.com>
2015-09-05 13:27:19 -0400

    UP

--- js/layout.js
+++ js/layout.js
@@ -19,98 +19,127 @@ var view = {
         var bottomButton = $('#bottom-button');
         var leftButton = $('#left-button');
         var container = $('#main-container');
-
-        topButton.on('click', function() {
-            container.animate({
-                scrollTop: 0
-            }, view.speed);
-        });
-        rightButton.on('click', function() {
-            container.animate({
-                scrollLeft: 2 * view.width
-            }, view.speed);
-        });
-        bottomButton.on('click', function() {
-            container.animate({
-                scrollTop: 2 * view.height
-            }, view.speed);
-        });
-        leftButton.on('click', function() {
-            container.animate({
-                scrollLeft: 0
-            }, view.speed);
-            card.addClass('flip');
-        });
-    },
-    initializeKeyBoard: function() {
-        var container = $('#main-container');
         var card = $('.card');
         var timeline = $('.timelinest');
 
-        $(document).on('keydown', function(e) {
-            var x = e.which || e.keyCode;
-            switch (x) {
-                //up
-                case 38:
-                    view.top = (view.top === 0 || view.left !== 1) ? view.top : view.top - 1;
-                    container.animate({
-                        scrollTop: view.top * view.height
-                    }, {
-                        duration: view.speed,
-                        easing: 'easeOutExpo'
-                    });
-                    break;
-                case 37:
-                    view.left = (view.left === 0 || view.top !== 1) ? view.left : view.left - 1;
-                    container.animate({
-                        scrollLeft: view.left * view.width
-                    }, {
-                        duration: view.speed,
-                        easing: 'easeOutExpo',
-                        complete: function() {
-                            if (!card.hasClass('flip')) {
-                                card.addClass('flip');
-                            }
-                            if(timeline.hasClass('svgLoaded')){
-                                $('.svgLoaded svg').remove();
-                                timeline.removeClass('svgLoaded');
-                            }
-                        },
-                    });
-                    break;
-                case 40:
-                    view.top = (view.top === 2 || view.left !== 1) ? view.top : view.top + 1;
-                    container.animate({
-                        scrollTop: view.top * view.height
-                    }, {
-                        duration: view.speed,
-                        easing: 'easeOutExpo'
-                    });
-                    break;
-                case 39:
-                    view.left = (view.left === 2 || view.top !== 1) ? view.left : view.left + 1;
-                    container.animate({
-                        scrollLeft: view.left * view.width
-                    }, {
-                        duration: view.speed,
-                        easing: 'easeOutExpo',
-                        complete: function() {
-                            if (card.hasClass('flip')) {
-                                card.removeClass('flip');
-                            }
-                            timeline.load('../images/timeline.svg', function(response) {
-                                $(this).addClass("svgLoaded");
-                                console.log(1);
-                                if (!response) {
-                                    // 加载失败的处理代码
-                                    console.log('error');
-                                }
-                            });
+        topButton.on('click', function() {
+            view.top = (view.top === 0 || view.left !== 1) ? view.top : view.top - 1;
+            container.animate({
+                scrollTop: view.top * view.height
+            }, {
+                duration: view.speed,
+                easing: 'easeOutExpo'
+            });
+            view.controlButtons();
+        });
+        rightButton.on('click', function() {
+            view.left = (view.left === 2 || view.top !== 1) ? view.left : view.left + 1;
+            container.animate({
+                scrollLeft: view.left * view.width
+            }, {
+                duration: view.speed,
+                easing: 'easeOutExpo',
+                complete: function() {
+                    if (card.hasClass('flip')) {
+                        card.removeClass('flip');
+                    }
+                    timeline.load('../images/timeline.svg', function(response) {
+                        $(this).addClass("svgLoaded");
+                        if (!response) {
+                            // 加载失败的处理代码
+                            console.log('error');
                         }
                     });
-                    break;
-            }
+                }
+            });
+            view.controlButtons();
         });
+        bottomButton.on('click', function() {
+            view.top = (view.top === 2 || view.left !== 1) ? view.top : view.top + 1;
+            container.animate({
+                scrollTop: view.top * view.height
+            }, {
+                duration: view.speed,
+                easing: 'easeOutExpo'
+            });
+            view.controlButtons();
+        });
+        leftButton.on('click', function() {
+            view.left = (view.left === 0 || view.top !== 1) ? view.left : view.left - 1;
+            container.animate({
+                scrollLeft: view.left * view.width
+            }, {
+                duration: view.speed,
+                easing: 'easeOutExpo',
+                complete: function() {
+                    if (!card.hasClass('flip')) {
+                        card.addClass('flip');
+                    }
+                    if (timeline.hasClass('svgLoaded')) {
+                        $('.svgLoaded svg').remove();
+                        timeline.removeClass('svgLoaded');
+                    }
+                },
+            });
+            view.controlButtons();
+        });
+    },
+    controlButtons: function() {
+        var topButton = $('#top-button');
+        var rightButton = $('#right-button');
+        var bottomButton = $('#bottom-button');
+        var leftButton = $('#left-button');
+        //page 1
+        if (view.left === 1 && view.top === 0) {
+            topButton.hide();
+            rightButton.hide();
+            leftButton.hide();
+            bottomButton.show();
+            bottomButton.find('h3').html('Home');
+            bottomButton.find('.icon').css('fill','#facb33');
+        }
+        //page 2
+        if (view.left === 0 && view.top === 1) {
+            topButton.hide();
+            rightButton.show();
+            leftButton.hide();
+            bottomButton.hide();
+            rightButton.find('h3').html('Home');
+            rightButton.find('.icon').css('fill','#e56292');
+        }
+        //page 3
+        if (view.left === 1 && view.top === 1) {
+            topButton.show();
+            rightButton.show();
+            leftButton.show();
+            bottomButton.show();
+            topButton.find('h3').html('Blog');
+            rightButton.find('h3').html('About Me');
+            leftButton.find('h3').html('Contact');
+            bottomButton.find('h3').html('Project');
+            topButton.find('.icon').css('fill','#2e2f34');
+            leftButton.find('.icon').css('fill','#2e2f34');
+            rightButton.find('.icon').css('fill','#2e2f34');
+            bottomButton.find('.icon').css('fill','#2e2f34');
+        }
+        //page 4
+        if (view.left === 2 && view.top === 1) {
+            topButton.hide();
+            rightButton.hide();
+            leftButton.show();
+            bottomButton.hide();
+            leftButton.find('h3').html('Home');
+            leftButton.find('.icon').css('fill','#2c5379');
+        }
+        //page 5
+        if (view.left === 1 && view.top === 2) {
+            topButton.show();
+            rightButton.hide();
+            leftButton.hide();
+            bottomButton.hide();
+            topButton.find('h3').html('Home');
+            topButton.find('.icon').css('fill','#48a075');
+        }
     },
     resize: function() {
         $(window).resize(function() {
@@ -130,7 +159,7 @@ var view = {
         view.initializeDiv();
         view.onloadAnimation();
         view.initializeNavButtons();
-        view.initializeKeyBoard();
+        //view.initializeKeyBoard();
         view.resize();
     }
 };
